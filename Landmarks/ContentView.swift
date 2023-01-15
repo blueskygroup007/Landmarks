7f9c5aca777f002724352771cc7ac8742da8f8ce
bluesskymac <blueskymac@123.com>
2023-01-16 00:41:06 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 李海永 on 2023/1/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            Text("Turtle Rock").font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California").font(.subheadline)
+            }
+            
+        }.padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 李海永 on 2023/1/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
